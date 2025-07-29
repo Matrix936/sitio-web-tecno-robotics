19ebea8a8b1753ac091a95b5cba3bf00fc42d5f0
Matrix936 <aguilargonzalezluismiguel8@gmail.com>
2025-07-29 16:54:09 -0600

    Prueba de cambios en rama feature/pruebas 2

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started putitos by editing <code>src/app/page.js</code>.
+            Get started putitos como Nanci by editing <code>src/app/page.js</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
