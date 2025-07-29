c0269552aa358906a577b8b6807004c91352e44a
Matrix936 <aguilargonzalezluismiguel8@gmail.com>
2025-07-29 16:19:57 -0600

    Prueba de cambios en rama feature/pruebas

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started putitos by editing <code>src/app/page.js</code>.
+            Get started putitos dobles by editing <code>src/app/page.js</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
